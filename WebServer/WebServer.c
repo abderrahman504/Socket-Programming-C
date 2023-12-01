1f33b6328378e6a526c27d9161f5e3fc954b33dd
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-01 18:38:07 +0200

    Testing threads in windows

--- WebServer/WebServer.c
+++ WebServer/WebServer.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <windows.h>
+#include <process.h>
+#include "Thread.c"
 
 
 int main(char args[])
 {
     printf("Web server running.\n");
+    ConnectionArgs t_args = {80};
+    _beginthread(connection, 0, &t_args);
 
+    Sleep(100);
     printf("Web server shutting down.\n");
     return 0;
 }
