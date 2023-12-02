1de22943c6701ba4563896d20b347775556e7d59
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-02 20:14:56 +0200

    In progress connection thread The connection thread listens continuously for requests from the client. If any error occurs in recieving data from the client the connection is terminated (Shutdown should be called to terminate the connection from the client side too). The request is parsed to extract the method and options. They are printed. In case of GET, the file path is extracted too. If the requested file can't be opened then a 404 Not found is sent to the client. Any errors with the sending process terminates the connection. What happens if the file is found is not handled yet. Handling POSTs is not implemented yet.

--- WebServer/Connection.c
+++ WebServer/Connection.c
@@ -2,9 +2,15 @@
 #include <ws2tcpip.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define BUFFER_LENGTH 1000
+
+#define NOT_FOUND "HTTP/1.1 404 Not Found\r\n"
+#define OK "HTTP/1.1 200 OK\r\n"
+#define BLANK_LINE "\r\n"
+
 
 typedef struct {
     ConnectionArgs* th_args;
@@ -14,6 +20,7 @@ typedef struct {
 typedef struct {
     SOCKET* socket;
     clock_t last_request;
+    char closed;
 }ConnectionArgs;
 
 
@@ -21,79 +28,148 @@ void connection(void* args)
 {
     ConnectionArgs* c_args = args;
     c_args->last_request = clock();
+    c_args->closed = 0;
     printf("Connection established with a client...\n");
     char recv_buf[BUFFER_LENGTH];
 
-    //Waiting for data from client
+    //Waiting for a request from the client
     while(1)
     {
         int recieved = recv(c_args->socket, recv_buf, BUFFER_LENGTH, 0);
         if (recieved >= 0)
         {
+            
             printf("Bytes received: %d\n", recieved);
-
-            int iSendResult = send(c_args->socket, recv_buf, recieved, 0 );
-            if (iSendResult == SOCKET_ERROR) {
-                printf("echo failed with error: %d\n", WSAGetLastError());
-                closesocket(c_args->socket);
-                return;
+            if(recieved > 0)
+            {
+                c_args->last_request = clock();
+                int send_result = handle_request(recv_buf, c_args->socket);
+                if (send_result == SOCKET_ERROR)
+                {
+                    printf("response failed with error: %d\n", WSAGetLastError());
+                    closesocket(c_args->socket);
+                    c_args->closed = 1;
+                    return;
+                }
             }
-            printf("Bytes sent: %d\n", iSendResult);
+
+            //Handling request
         }
         else
         {
             printf("recv failed with error: %d\n", WSAGetLastError());
             closesocket(c_args->socket);
+            c_args->closed = 1;
             return;
         }
     }
 }
 
 
-
-//Parses a HTTP request sent by a client.
-//Returns a string containing a requested file, or NULL incase of a POST request
-char* parse_request(char* request)
+//-----------------------REQUEST PARSING--------------------------//
+//Parses a HTTP request sent by a client and prints the type and options.
+// method and path are output pointers 
+//method contains "GET" or "POST".
+//path contains path of the requested file in case of GET.
+void parse_request(char* request, char* method, char* path)
 {
     //Parsing the request line
     char* request_line = strtok(request, "\n");
-    char *method, *url, *version;
-    parse_rqln(request_line, method, url, version);
+    char* url;
+    //Extract method and url from request line
+    parse_rqln(request_line, method, url);
+
+    char get[] = "GET";
+    if (strcmp(method, get) == 0)
+    {
+        path = (char*) malloc(32);
+        sscanf(url, "/%s", path);
+    }
     
+
+    free(url);
     //Parsing the request options
-    parse_optns(request);
+    print_optns(request);
 }
 
 
-// Parses a request line and outputs method, url, and http version.
+// Parses a request line and outputs method and url.
 // the output pointers are allocated by this function, so don't allocate them beforehand.
-void parse_rqln(char* request_line, char* method, char* url, char* version)
+void parse_rqln(char* request_line, char* method, char* url)
 {
     method = (char*) malloc(5);
     url = (char*) malloc(64);
-    version = (char*) malloc(16);
-    sscanf(request_line, "%s %s %s", method, url, version);
-    printf("%s\n", method);
+    // char version[16];
+    sscanf(request_line, "%s %s", method, url);
+    // printf("%s\n", method);
 }
-
-
-void parse_optns(char* request)
+/*
+Prints the request options and blank line.
+*/
+void print_optns(char* request)
 {
     char* next_optn = strtok(NULL, "\n");
     while(next_optn != NULL)
     {
-        if (next_optn[0] == '\r')
-            break;
+        // if (next_optn[0] == '\r')
+        //     break;
         printf("%s\n", next_optn);
         next_optn = strtok(NULL, "\n");
     }
-    printf("\n");
+    // printf("\n");
 }
 
 
-void free_rqln(char* method, char* url, char* version)
+void free_rqln(char* method, char* path)
 {
     free(method);
-    free(url);
-    free(version);
+    free(path);
 }
+//------------------------END OF REQUEST PARSING--------------------------//
+//---------------------------REQUEST HANDLING----------------------------//
+/*
+Handles request in buffer
+*/
+int handle_request(char buffer[], SOCKET* socket)
+{
+    char* method, path;
+    //Parse and print the request and options
+    parse_request(buffer, method, path);
+    char get[] = "GET";
+    int result;
+    if (strcmp(method, get) == 0) //GET request
+    {
+        result = handle_get(socket, path);
+        free_rqln(method, path);
+    }
+    else //POST request
+    {
+        result = handle_post(socket);
+    }
+    return result;
+}
+
+int handle_get(SOCKET* socket, char* path)
+{
+    printf("Opening %s...\n", path);
+    FILE *fh = fopen(path, "r");
+    if (fh == NULL) //File not found
+    {
+        printf("Failed to open file\n");
+        char response[100];
+        strcat(response, NOT_FOUND);
+        strcat(response, BLANK_LINE);
+        return send(*socket, response, 100, 0);
+    }
+    else //File found
+    {
+        //implement later
+    }
+}
+
+int handle_post(SOCKET* socket)
+{
+    //implement later
+}
+
+//---------------------------END OF REQUEST HANDLING----------------------------//
