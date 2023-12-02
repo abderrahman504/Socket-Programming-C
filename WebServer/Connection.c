b17423714091fd6903cc5aecda05655d765cb4c4
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-02 16:56:24 +0200

    Method for parsing and printing request parse_request() takes a request string and parses it, printing the request type, then each option on a line, then a blank line.

--- WebServer/Connection.c
+++ WebServer/Connection.c
@@ -1,5 +1,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <string.h>
+#include <stdio.h>
 
 typedef struct {
     long long time_since_last_request;
@@ -19,4 +21,53 @@ void connection(void* args)
 
 
     printf("Connection closing\n");
+}
+
+
+
+//Parses a HTTP request sent by a client.
+//Returns a string containing a requested file, or NULL incase of a POST request
+char* parse_request(char* request)
+{
+    //Parsing the request line
+    char* request_line = strtok(request, "\n");
+    char *method, *url, *version;
+    parse_rqln(request_line, method, url, version);
+    
+    //Parsing the request options
+    parse_optns(request);
+}
+
+
+// Parses a request line and outputs method, url, and http version.
+// the output pointers are allocated by this function, so don't allocate them beforehand.
+void parse_rqln(char* request_line, char* method, char* url, char* version)
+{
+    method = (char*) malloc(5);
+    url = (char*) malloc(64);
+    version = (char*) malloc(16);
+    sscanf(request_line, "%s %s %s", method, url, version);
+    printf("%s\n", method);
+}
+
+
+void parse_optns(char* request)
+{
+    char* next_optn = strtok(NULL, "\n");
+    while(next_optn != NULL)
+    {
+        if (next_optn[0] == '\r')
+            break;
+        printf("%s\n", next_optn);
+        next_optn = strtok(NULL, "\n");
+    }
+    printf("\n");
+}
+
+
+void free_rqln(char* method, char* url, char* version)
+{
+    free(method);
+    free(url);
+    free(version);
 }
