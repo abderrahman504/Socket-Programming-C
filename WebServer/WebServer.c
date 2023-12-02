8dd31f326c2de78a8dfeef7ea5378d04eb0ef8b8
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-02 15:54:49 +0200

    Creating a listening socket

--- WebServer/WebServer.c
+++ WebServer/WebServer.c
@@ -1,16 +1,71 @@
 #include <stdio.h>
-#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <process.h>
 #include "Thread.c"
 
+#define DEFAULT_PORT "60000"
+
+
+WSADATA wsaData;
 
 int main(char args[])
 {
-    printf("Web server running.\n");
-    ConnectionArgs t_args = {80};
-    _beginthread(connection, 0, &t_args);
+    // Initialize Winsock
+    int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+    printf("WSAStartup failed: %d\n", iResult);
+    return 1;
+    }
 
-    Sleep(100);
-    printf("Web server shutting down.\n");
+    //Preparing to create a socket
+    struct addrinfo *result = NULL, *ptr = NULL, hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+    printf("getaddrinfo failed: %d\n", iResult);
+    WSACleanup();
+    return 1;
+    }
+
+    //Creating a listening socket
+    SOCKET ListenSocket = INVALID_SOCKET;
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    //Binding the socket
+    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+    freeaddrinfo(result);
+
+    //Listening on the socket
+    if ( listen( ListenSocket, SOMAXCONN ) == SOCKET_ERROR ) {
+        printf( "Listen failed with error: %ld\n", WSAGetLastError() );
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+    printf("Listening on port %s\n", DEFAULT_PORT);
+
+    // ConnectionArgs t_args = {80};
+    // _beginthread(connection, 0, &t_args);
+    // Sleep(100);
+    printf("Web server closing\n");
     return 0;
 }
